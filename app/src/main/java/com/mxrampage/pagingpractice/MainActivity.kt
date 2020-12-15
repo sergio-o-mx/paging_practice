a6f3ce2588dca5636671afa0ffdf9979d818b9f0
sergio-o-mx <xrampage@gmail.com>
2020-12-15 14:06:55 -0600

    Migrated from fragment to fragmentcontainerview, added code to avoid an issue with the change

--- app/src/main/java/com/mxrampage/pagingpractice/MainActivity.kt
+++ app/src/main/java/com/mxrampage/pagingpractice/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.mxrampage.pagingpractice
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController = this.findNavController(R.id.navHostFragment)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
